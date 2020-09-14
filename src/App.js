75bac71b0d815ed16c46b3fe2956a76ec14536ef
Tigran <tig.vardanyan.001@gmail.com>
2020-09-15 01:25:15 +0400

    56

--- src/App.js
+++ src/App.js
@@ -52,22 +52,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}/>
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={() => this.switchNameHandler()}/>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-            changed={this.nameChangedHandler}/>
-          <Person
-            name={this.state.persons[3].name}
-            age={this.state.persons[3].age}>
-            Props.children
-          </Person>
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name}
+              age={person.age}/>
+          })}
         </div>
       )
     }
