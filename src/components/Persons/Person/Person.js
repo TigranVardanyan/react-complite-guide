2a2c7a7a8691530a660748b7deca5f643eec2741
Tigran <tig.vardanyan.001@gmail.com>
2020-09-18 03:47:20 +0400

    component update lifecycle

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,15 +3,18 @@ import classes from './Person.css'
 
 
 
-const person = (props) => {
-  console.log('[Person.js] rendering...');
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I'm a {props.name}, my age is {props.age}</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.change} defaultValue={props.name}/>
-    </div>
-  )
-};
+class Person extends Component {
+  render() {
+    console.log('[Person.js] rendering...');
 
-export default person;
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I'm a {this.props.name}, my age is {this.props.age}</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.change} defaultValue={this.props.name}/>
+      </div>
+    )
+  }
+}
+
+export default Person;
