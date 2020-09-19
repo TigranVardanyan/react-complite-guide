fab13ed7bde7653de49f6aea362a887137dae794
Tigran <tig.vardanyan.001@gmail.com>
2020-09-20 02:36:42 +0400

    Ref's

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 import classes from './Person.css'
-
 import PropTypes from 'prop-types'
-
 import Aux from '../../../hoc/Auxiliary'
 import withClass from "../../../hoc/withClass";
+
 class Person extends Component {
+  constructor(props) {
+    super();
+    this.inputElementRef = React.createRef();
+  }
+  componentDidMount() {
+    //this.inputElement.focus()
+    this.inputElementRef.current.focus();
+  }
+
 
   render() {
     console.log('[Person.js] rendering...');
@@ -18,6 +26,10 @@ class Person extends Component {
         <p key={'iab'}>{this.props.children}</p>
         <input
           key={'iac'}
+          //ref={(inputEl) => {
+          //  this.inputElement = inputEl
+          //}}
+          ref={this.inputElementRef}
           type="text"
           onChange={this.props.changed}
           defaultValue={this.props.name}
@@ -26,11 +38,11 @@ class Person extends Component {
     )
   }
 }
+
 Person.propTypes = {
   click: PropTypes.func,
-  name:PropTypes.string,
-  age:PropTypes.number,
-  changed:PropTypes.func
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
 };
-
 export default withClass(Person, classes.Person);
