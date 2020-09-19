4a9135fc05fe4ac486bbca9f01e82756a1d148a1
Tigran <tig.vardanyan.001@gmail.com>
2020-09-20 01:09:37 +0400

    Rendering Adjacent JSX Elements == react fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import classes from './Person.css'
 
+import Aux from '../../../hoc/Auxiliary'
 
 class Person extends Component {
 
@@ -21,12 +22,31 @@ class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     //console.log('Person.props', this.props);
+    //return [
+    //    <p key={'iaa'} onClick={this.props.click}>
+    //      I'm a {this.props.name}, my age is {this.props.age}
+    //    </p>,
+    //    <p key={'iab'}>{this.props.children}</p>,
+    //    <input
+    //      key={'iac'}
+    //      type="text"
+    //      onChange={this.props.change}
+    //      defaultValue={this.props.name}
+    //    />
+    //]
     return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>I'm a {this.props.name}, my age is {this.props.age}</p>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.change} defaultValue={this.props.name}/>
-      </div>
+      <Aux>
+        <p key={'iaa'} onClick={this.props.click}>
+          I'm a {this.props.name}, my age is {this.props.age}
+        </p>
+        <p key={'iab'}>{this.props.children}</p>
+        <input
+          key={'iac'}
+          type="text"
+          onChange={this.props.change}
+          defaultValue={this.props.name}
+        />
+      </Aux>
     )
   }
 }
